1a7ec281e9550fdb79edabb4dde530efa61695c4
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-02 23:32:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var inputs = document.querySelectorAll('.code');
 
 for(let i =0; i < inputs.length; i++){
 	inputs[i].addEventListener('input', function() {
-		if(this.value.length == 1){
-			if(i != inputs.length -1){
+		if(this.value != ''){
+			if(i < inputs.length -1){
 				inputs[i+1].focus();
 			}
 		}
@@ -12,7 +12,7 @@ for(let i =0; i < inputs.length; i++){
 
 	inputs[i].addEventListener('keydown', function (e) {
 		if(e.key === 'Backspace' && this.value.length == 0){
-			if (i != 0){
+			if (i > 0){
 				inputs[i-1].focus();
 			}
 		}
