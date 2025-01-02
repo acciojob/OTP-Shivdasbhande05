a75f7c187f4cb396783b9533c38efa1e910015df
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-02 22:55:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+var inputs = document.querySelectorAll('.otp');
+
+for(let i =0; i < inputs.length; i++){
+	inputs[i].addEventListener('input', function() {
+		if(this.value.length == 1){
+			if(i != inputs.length -1){
+				inputs[i+1].focus();
+			}
+		}
+	});
+
+	inputs[i].addEventListener('keydown', function (e) {
+		if(e.key === 'Backspace' && this.value.length == 0){
+			if (i != 0){
+				inputs[i-1].focus();
+			}
+		}
+	});
+}
